2c16efb0d29580833310988eca18bdf77bbb766e
Amarjit singh <amarjitsingh52922@gmail.com>
2023-02-24 00:28:14 +0530

    day 11 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:day2/models/catalog.dart';
 import 'package:day2/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,14 +10,23 @@ class HomePage extends StatelessWidget {
   final String name = "Amarjit Singh";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(6, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Catalog App",
         ),
       ),
-      body: Center(
-        child: Text("Welcome to my  $days of Flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: const MyDrawer(),
     );
