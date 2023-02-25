a599276704c2cc4fbd40fcda8ab4da2990bbbcaf
Amarjit singh <amarjitsingh52922@gmail.com>
2023-02-26 01:58:12 +0530

    completed

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -14,6 +14,7 @@ class ItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () {},
         leading: Image.network(item.image),
         title: Text(item.name),
         subtitle: Text(item.desc),
